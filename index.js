de2866a4d8889908c4578ce219c4b02801ac1f13
Maurice Mohlek <mohlek@gmail.com>
2015-12-22 09:47:36 +0100

    init

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 /* jshint node: true */
 'use strict';
+let basePlugin = require('ember-cli-deploy-plugin');
 
 module.exports = {
-  name: 'ember-cli-deploy-ftp'
+  name: 'ember-cli-deploy-ftp',
+
+  createDeployPlugin(options) {
+    let deployPlugin = basePlugin.extend({
+      name: options.name,
+
+      defaultConfig: {
+        host: '127.0.0.1'
+      },
+
+      didBuild(context) {
+      },
+      upload(context) {
+        this.log('Upload to FTP');
+      },
+      didDeploy(context) {
+      }
+    });
+
+    return new deployPlugin();
+  }
 };
