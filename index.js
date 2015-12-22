ab65c62a02565b8a5afd7d45f7979fb8332dfef3
Maurice Mohlek <mohlek@gmail.com>
2015-12-22 13:37:03 +0100

    connect that sucker

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* jshint node: true */
 'use strict';
+var promise = require('ember-cli/lib/ext/promise');
 var basePlugin = require('ember-cli-deploy-plugin');
 var minimatch = require('minimatch');
 var ftp = require('./lib/ftp');
@@ -11,30 +12,37 @@ module.exports = {
     var deployPlugin = basePlugin.extend({
       name: options.name,
 
+      requiredConfig: ['host', 'user', 'pass'],
       defaultConfig: {
         filePattern: '**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,ttf,woff,woff2}',
+        port: 21,
         distFiles(context) {
           return context.distFiles || [];
         }
-      },
-
-      didBuild(context) {
       },
       upload(context) {
         var filePattern = this.readConfig('filePattern');
         var distDir = this.readConfig('distDir');
         var distFiles = this.readConfig('distFiles');
 
+        var host = this.readConfig('host');
+        var port = this.readConfig('port');
+        var user = this.readConfig('user');
+        var pass = this.readConfig('pass');
+
         var filesToUpload = distFiles.filter(minimatch.filter(filePattern, {matchBase: true}));
 
         ftp = new ftp();
-        ftp.upload({host: 'test'});
-        ftp.upload({host: 'test'});
+        ftp.connect({host, port, user, pass}).then(()=> {
+          console.log('connected :)');
+          ftp.upload(filesToUpload);
+        }).catch(this._errorMessage.bind(this));
       },
-      didDeploy(context) {
+      _errorMessage(error) {
+        this.log(error, {color: 'red'});
+        return promise.reject(error);
       }
     });
-
     return new deployPlugin();
   }
 };
